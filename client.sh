7d6309a4b05a3a832f2bb01d768c9837cdab8785
Nikolay Plotnik <nikolay.plotnik@gmail.com>
2015-06-15 13:59:36 +0300

    Add pgbench load

--- client.sh
+++ client.sh
@@ -8,12 +8,13 @@ requested_ct=$START_CT_NUM
 while true; do
     echo $requested_ct | nc -vv -l -p $PORT
     echo "Send $requested_ct containers to server"
-    ips=$(echo "Test" | nc -v -l -p $PORT)
-    echo "Receive $ips"
-    echo "Loading..."
-    sleep 40
+    ips=($(echo "Test" | nc -v -l -p $PORT))
+    echo "Receive ${ips[@]}"
+    for ip in ${ips[@]}
+    do
+        pgbench -h $ip -c 100 -T 3600
+    done &&
     nc -v 192.168.3.2 $PORT
-    echo "End"
     requested_ct=$((requested_ct+1))
 done
 
